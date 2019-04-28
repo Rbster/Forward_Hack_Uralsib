420d5bc7a97bbe5c6cef705713210723169c620d
Dmitry7571 <46320055+Dmitry7571@users.noreply.github.com>
2019-04-28 18:04:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,11 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 def start(bot, update):
-    # подробнее об объекте update: https://core.telegram.org/bots/api#update
-    msg = 'Здравствуйте, Вы обратились к боту помощнику банка Уралсиб \
-           Для помощи выберите команду /help \
-           Но вначала расскажите, где вы находитесь? \
-           так мы сможем понять, как вам помочь :)'
+    msg = 'Здравствуйте, Вы обратились к боту помощнику банка Уралсиб \nДля помощи выберите команду /help \nНо вначала расскажите, где вы находитесь? \nТак мы сможем понять, как вам помочь :)'
 
     reply_kb_markup = telegram.ReplyKeyboardMarkup([
                                                     [telegram.KeyboardButton('Поделиться геопозицией', 
@@ -39,18 +35,82 @@ def help(bot, update):
 
 def support(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Как Вы хотите связаться с техподдержкой? Есть два варианта:\n1)/number - прислать Вам номер телефона специалиста\n2)/link - скидываем сюда ссылку на телеграм канал техподдержки\n!")
+
 def link(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="ссылка:<ссылка>")
+    
 def number(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="88002505757")
     
 def helpme(bot, update):
     # подробнее об объекте update: https://core.telegram.org/bots/api#update
     bot.sendMessage(chat_id=update.message.chat_id, text="Чем я могу Вам помочь?\n\n1)Подобрать для Вас подходящую услугу\n2)Связать Вас с техподдержкой.\nВведите просто цифру как команду(например:/1)!") 
+
 def two(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="<ссылка>")
+    
 def one(bot,update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="В каком городе(поселке) Вы находитесь?")
+    msg="Какое Вы лицо:Физическое или юридическое?"
+    kb_markup = telegram.ReplyKeyboardMarkup([
+                                                    [telegram.KeyboardButton('Физическое', 
+                                                                             reply_msg='Физическое')],
+                                                    [telegram.KeyboardButton('Юридическое', 
+                                                                             reply_msg='Юридическое')]
+                                                    ],
+                                                   resize_keyboard=True,
+                                                   one_time_keyboard=True)
+
+   # print(update.user_id)
+
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text=msg, 
+                    reply_markup=kb_markup)
+def yuri(bot, update):
+    kb_markup = telegram.ReplyKeyboardMarkup([
+                                                    [telegram.KeyboardButton('Заплатить за Интернет', 
+                                                                             reply_msg='Заплатить за Интернет')],
+                                                    [telegram.KeyboardButton('Заплатить за коммуналку', 
+                                                                             reply_msg='Заплатить за коммуналку')],
+                                                    [telegram.KeyboardButton('Заплатить за ТВ', 
+                                                                             reply_msg='Заплатить за ТВ')],
+                                                    [telegram.KeyboardButton('Заплатить за телефон', 
+                                                                             reply_msg='Заплатить за телефон')],
+                                                    [telegram.KeyboardButton('Заплатить за образование', 
+                                                                             reply_msg='Заплатить за образование')],
+                                                    [telegram.KeyboardButton('Заплатить за систему безопасности', 
+                                                                             reply_msg='Заплатить за систему безопасности')],
+                                                    ],
+                                                   resize_keyboard=True,
+                                                   one_time_keyboard=True)
+   # print(update.user_id)
+
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text=msg, 
+                    reply_markup=kb_markup)
+
+def fiz(bot, update):
+    kb_markup = telegram.ReplyKeyboardMarkup([
+                                                    [telegram.KeyboardButton('Заплатить за Интернет', 
+                                                                             reply_msg='Заплатить за Интернет')],
+                                                    [telegram.KeyboardButton('Заплатить за коммуналку', 
+                                                                             reply_msg='Заплатить за коммуналку')],
+                                                    [telegram.KeyboardButton('Заплатить за ТВ', 
+                                                                             reply_msg='Заплатить за ТВ')],
+                                                    [telegram.KeyboardButton('Заплатить за телефон', 
+                                                                             reply_msg='Заплатить за телефон')],
+                                                    [telegram.KeyboardButton('Получить кредит', 
+                                                                             reply_msg='Получить кредит')],
+                                                    [telegram.KeyboardButton('Пополнить счет', 
+                                                                             reply_msg='Пополнить счет')],
+                                                    ],
+                                                   resize_keyboard=True,
+                                                   one_time_keyboard=True)
+   # print(update.user_id)
+
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text=msg, 
+                    reply_markup=kb_markup)
+
 
 # def get_location(bot, update):
 #     target_latitude = update.message.location.latitude
@@ -74,7 +134,7 @@ def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
-updater = Updater(token='793876198:AAHzznrZmwcmzcbFJKNDN9DJbxXXBUnzNvY')  # тут токен, который выдал вам Ботский Отец!
+updater = Updater(token='702809746:AAHmIm6mPpT1TLmkw1RqxsYF4SUfP2ebPqk')  # тут токен, который выдал вам Ботский Отец!
 
 start_handler = CommandHandler('start', start)  # этот обработчик реагирует только на команду /start
 helpme_handler = CommandHandler('helpme', helpme)  # этот обработчик реагирует только на команду /helpme
@@ -84,6 +144,8 @@ support_handler = CommandHandler('support', support)
 link_handler = CommandHandler('link', link)
 number_handler = CommandHandler('number', number)
 one_handler = CommandHandler('1', one)
+yuri_handler = CommandHandler('Юридическое', yuri)
+fiz_handler = CommandHandler('Юридическое', fiz)
 
 
 
@@ -98,6 +160,8 @@ dp.add_handler(support_handler)   # регистрируем в госреест
 dp.add_handler(link_handler)   # регистрируем в госреестре обработчиков
 dp.add_handler(number_handler)   # регистрируем в госреестре обработчиков
 dp.add_handler(one_handler)   # регистрируем в госреестре обработчиков
+dp.add_handler(yuri_handler)   # регистрируем в госреестре обработчиков
+dp.add_handler(fiz_handler)   # регистрируем в госреестре обработчиков
 
 ###dp.add_handler(CallbackQueryHandler(callbackHandler, pass_chat_data=True))
 
