3098cc163017a87c0751c0c747f939fa8e3a2565
Rbster <rbster@yandex.ru>
2019-04-28 14:06:37 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,38 @@
 # -*- coding: utf-8 -*-                                                                                                      
-from telegram.ext import Updater         # пакет называется python-telegram-bot, но Python-модуль почему-то просто telegram ¯\_(ツ)_/¯
-from telegram.ext import CommandHandler  
+# пакет называется python-telegram-bot, но Python-модуль почему-то просто telegram ¯\_(ツ)_/¯
+
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler,\
+                         RegexHandler, Filters
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
 
 def start(bot, update):
     # подробнее об объекте update: https://core.telegram.org/bots/api#update
-    bot.sendMessage(chat_id=update.message.chat_id, text="Здравствуйте. Введите /help, чтобы посмотреть функционал данного бота.")
+    msg = 'Здравствуйте, Вы обратились к боту помощнику банка Уралсиб \
+           Для помощи выберите команду /help \
+           Но вначала расскажите, где вы находитесь? \
+           так мы сможем понять, как вам помочь :)'
+
+    reply_kb_markup = telegram.ReplyKeyboardMarkup([
+                                                    [telegram.KeyboardButton('Поделиться геопозицией', 
+                                                                             request_location=True)]
+                                                    ],
+                                                   resize_keyboard=True,
+                                                   one_time_keyboard=True)
+   # print(update.user_id)
+
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text=msg, 
+                    reply_markup=reply_kb_markup)
+
 
 def help(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Для Вас, у нас есть несколько команд, результаты которых, надеемся помогут Вам.\n1)/helpme - помогаем мы \n2)/support - по Вашему выбору скидываем в этот чат ссылку на телеграм канал (техподдержки) или телефон квалифицированного специалиста техподдержки")
@@ -23,9 +51,30 @@ def two(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="<ссылка>")
 def one(bot,update):
     bot.sendMessage(chat_id=update.message.chat_id, text="В каком городе(поселке) Вы находитесь?")
-    
 
-updater = Updater(token='702809746:AAHmIm6mPpT1TLmkw1RqxsYF4SUfP2ebPqk')  # тут токен, который выдал вам Ботский Отец!
+# def get_location(bot, update):
+#     target_latitude = update.message.location.latitude
+#     target_longitude = update.message.location.longitude
+#     bot.sendMessage(chat_id=update.message.chat_id, text="Ваши координаты:" + target_latitude + ' ' + target_longitude)
+'''
+def callbackHandler(bot,update):
+    query = update.callback_query
+    username = update.effective_user.username
+    chat_id = query.message.chat.id
+    message_id = query.message.message_id
+    if int(query.data) == 1:
+        bot.sendPhoto(photo=getcat(),
+                      chat_id=chat_id,
+                      message_id=message_id,
+                      reply_markup=draw_button())
+'''
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+updater = Updater(token='793876198:AAHzznrZmwcmzcbFJKNDN9DJbxXXBUnzNvY')  # тут токен, который выдал вам Ботский Отец!
 
 start_handler = CommandHandler('start', start)  # этот обработчик реагирует только на команду /start
 helpme_handler = CommandHandler('helpme', helpme)  # этот обработчик реагирует только на команду /helpme
@@ -36,12 +85,25 @@ link_handler = CommandHandler('link', link)
 number_handler = CommandHandler('number', number)
 one_handler = CommandHandler('1', one)
 
-updater.dispatcher.add_handler(start_handler)   # регистрируем в госреестре обработчиков
-updater.dispatcher.add_handler(helpme_handler)   # регистрируем в госреестре обработчиков
-updater.dispatcher.add_handler(help_handler)   # регистрируем в госреестре обработчиков
-updater.dispatcher.add_handler(two_handler)   # регистрируем в госреестре обработчиков
-updater.dispatcher.add_handler(support_handler)   # регистрируем в госреестре обработчиков
-updater.dispatcher.add_handler(link_handler)   # регистрируем в госреестре обработчиков
-updater.dispatcher.add_handler(number_handler)   # регистрируем в госреестре обработчиков
-updater.dispatcher.add_handler(one_handler)   # регистрируем в госреестре обработчиков
+
+
+
+
+dp = updater.dispatcher
+dp.add_handler(start_handler)   # регистрируем в госреестре обработчиков
+dp.add_handler(helpme_handler)   # регистрируем в госреестре обработчиков
+dp.add_handler(help_handler)   # регистрируем в госреестре обработчиков
+dp.add_handler(two_handler)   # регистрируем в госреестре обработчиков
+dp.add_handler(support_handler)   # регистрируем в госреестре обработчиков
+dp.add_handler(link_handler)   # регистрируем в госреестре обработчиков
+dp.add_handler(number_handler)   # регистрируем в госреестре обработчиков
+dp.add_handler(one_handler)   # регистрируем в госреестре обработчиков
+
+###dp.add_handler(CallbackQueryHandler(callbackHandler, pass_chat_data=True))
+
+
+dp.add_error_handler(error)
+
 updater.start_polling()  # поехали!
+
+updater.idle()
